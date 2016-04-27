578537237d73599665a47fc358bcaa879103e991
aaron <aarongitis@gmail.com>
2016-04-26 20:25:00 -0700

    Add for loop structure to game method

--- app/src/main/java/co/aartea/tic_tac_toe/GameActivity.java
+++ app/src/main/java/co/aartea/tic_tac_toe/GameActivity.java
@@ -1,18 +1,163 @@
 package co.aartea.tic_tac_toe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 //Project notes: When 'X' is clicked; change color with dark blue text, light blue background
 //'O' is clicked -- display dark red text; light red background.
-public class GameActivity extends AppCompatActivity {
+//Refactor -- iterate buttons and setOnItemClickListeners();
+
+public class GameActivity extends AppCompatActivity{
+
+    t3Helper game = new t3Helper();
+    Button n1,n2,n3,n4,n5,n6,n7,n8,n9;
+
+    String position;                    //Stores a positional value via column/row array
+    String player1, player2;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //Add button listeners
+        n1 = (Button) findViewById(R.id.n1);
+        n2 = (Button) findViewById(R.id.n2);
+        n3 = (Button) findViewById(R.id.n3);
+        n4 = (Button) findViewById(R.id.n4);
+        n5 = (Button) findViewById(R.id.n5);
+        n6 = (Button) findViewById(R.id.n6);
+        n7 = (Button) findViewById(R.id.n7);
+        n8 = (Button) findViewById(R.id.n8);
+        n9 = (Button) findViewById(R.id.n9);
+
+        player1 = "X";      //mod2 == 0; even
+        player2 = "O";      //mod2 == 1; odd
+
+        n1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n1.setText(player1);
+                    game.boardState([0][1]);
+                }
+                else
+                    n1.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n2.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n2.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n3.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n3.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n4.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n4.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n5.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n5.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n6.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n6.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n7.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n7.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n8.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n8.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+        n9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter%2 == 0){
+                    n9.setText(player1);
+                    game.boardState(player1);
+                }
+                else
+                    n9.setText(player2);
+                    game.boardState(player2);
+            }
+        });
+
+//        //Pass seed of button clicked, this will start our game.
+//        Intent pass = new Intent();
+//        pass.putExtra("key", seed);
+
+
 
     }
 }
--- app/src/main/java/co/aartea/tic_tac_toe/MainActivity.java
+++ app/src/main/java/co/aartea/tic_tac_toe/MainActivity.java
@@ -8,38 +8,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText p1, p2;
-    TextView tv;
+
     Button startGame;
+    String s1, s2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //tv = (TextView) findViewById(R.id.)
         p1 = (EditText) findViewById(R.id.player1);
         p2 = (EditText) findViewById(R.id.player2);
+
         startGame = (Button) findViewById(R.id.gameStart);
-
-
 
         startGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s1,s2;
                 s1 = p1.getText().toString();
                 s2 = p2.getText().toString();
-                if(s1.isEmpty() || s2.isEmpty()){
+                //Validate name input
+                if(s1.equals("") || s2.equals("")){         //Android can't use toString. Because it will allow for "" and that's a string.
                     Toast.makeText(MainActivity.this, "Please enter a valid name!", Toast.LENGTH_SHORT).show();
                 }
-                Intent i = new Intent(MainActivity.this,GameActivity.class);
-                startActivity(i);
+                else {
+                    Intent i = new Intent(MainActivity.this, GameActivity.class);
+                    i.putExtra("p1", s1);
+                    i.putExtra("p2", s2);
+                    startActivity(i);
+                }
             }
         });
 
